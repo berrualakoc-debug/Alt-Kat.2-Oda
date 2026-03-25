2049a4c2f9e38b35642c99a7f33d854e55d3e1d9
berrualakoc-debug <berrualakoc@gmail.com>
2026-03-25 16:19:21 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gr",
+      "name": "GİRİŞ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6379507745428654,
+          "pitch": 0.456340096471731,
+          "rotation": 0,
+          "target": "1-gynme-alani"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-gynme-alani",
+      "name": "GİYİNME ALANI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.08994470924579012,
+        "pitch": 0.08081539878371835,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4832260875506229,
+          "pitch": 0.6403265406966305,
+          "rotation": 0,
+          "target": "2-gynme-alani-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gynme-alani-2",
+      "name": "GİYİNME ALANI-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22703210112271677,
+          "pitch": 0.5052977669970815,
+          "rotation": 0,
+          "target": "4-mnbar-alani"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-banyo",
+      "name": "BANYO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6537271544341579,
+          "pitch": 0.4987550247207224,
+          "rotation": 4.71238898038469,
+          "target": "0-gr"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-mnbar-alani",
+      "name": "MİNİBAR ALANI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.5400668045312287,
+        "pitch": 0.0034788914106265167,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.029319122860677282,
+          "pitch": 0.6285112316585248,
+          "rotation": 0,
+          "target": "3-banyo"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
